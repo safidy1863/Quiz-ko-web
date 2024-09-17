332b4a66c3c95a8d6b0cea5559f9706fe4c3efed
ROBUSTE Manohisafidy <robustemmanuel@gmail.com>
2024-09-17 03:55:18 +0300

    add all paths
    Signed-off-by: ROBUSTE Manohisafidy <robustemmanuel@gmail.com>

--- src/modules/dashboard/constants/paths-dashboard.constants.ts
+++ src/modules/dashboard/constants/paths-dashboard.constants.ts
@@ -2,6 +2,9 @@ export const pathsDashboard = {
   home: "home",
   students: "students",
   profile: "profile",
+  tests: "tests",
+  results: "results",
+  settings: "settings",
 };
 
 export const LEVEL_DYNAMIC_PATH = "level";
--- src/modules/dashboard/routes/dashboard.routes.tsx
+++ src/modules/dashboard/routes/dashboard.routes.tsx
@@ -1,6 +1,6 @@
 import { TRoute } from "@/types";
 import { LEVEL_DYNAMIC_PATH, pathsDashboard } from "../constants";
-import { Home, Students } from "../pages";
+import { Home, Profile, Results, Settings, Students, Tests } from "../pages";
 import { Link, Navigate } from "react-router-dom";
 
 export const DashboardRoutes: TRoute[] = [
@@ -13,6 +13,26 @@ export const DashboardRoutes: TRoute[] = [
     path: `${pathsDashboard.students}/:${LEVEL_DYNAMIC_PATH}`,
     element: <Students />,
     crumb: <Link to={pathsDashboard.home}>Students</Link>,
+  },
+  {
+    path: pathsDashboard.profile,
+    element: <Profile />,
+    crumb: <Link to={pathsDashboard.home}>Profile</Link>,
+  },
+  {
+    path: pathsDashboard.tests,
+    element: <Tests />,
+    crumb: <Link to={pathsDashboard.home}>Tests</Link>,
+  },
+  {
+    path: pathsDashboard.results,
+    element: <Results />,
+    crumb: <Link to={pathsDashboard.home}>Results</Link>,
+  },
+  {
+    path: pathsDashboard.settings,
+    element: <Settings />,
+    crumb: <Link to={pathsDashboard.home}>Settings</Link>,
   },
   {
     path: "",
